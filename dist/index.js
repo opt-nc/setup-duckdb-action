ab18b47ac14995cabf5c641a9de80ae1cf83815e
Michèle BARRE <michele.barre@opt.nc>
2023-03-17 11:11:38 +1100

    get version

--- dist/index.js
+++ dist/index.js
@@ -22,7 +22,8 @@ module.exports = async function () {
         //let version_final = version;
         const headers = {'Accept': 'application/vnd.github+json', 'X-GitHub-Api-Version': '2022-11-28'}
         const res = await axios({url: 'https://api.github.com/repos/duckdb/duckdb/releases/latest', headers});
-        core.info(`result data : ${res.data}`)
+        core.info(`result data : ${res.data.status}`)
+        core.info(`result data : ${res.data.tagName}`)
         if (res.data.status === 'failed' || res.data.status === 'cancelled') {
             core.error(`❌ Failed to get latest DuckDB version`);
             core.setFailed(res.data);
