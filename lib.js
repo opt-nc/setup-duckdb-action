d73065e19e5c9f57446d43b915dd1c4ffe56d94f
Michèle BARRE <michele.barre@opt.nc>
2023-03-17 20:40:14 +1100

    finalize duckdb install

--- lib.js
+++ lib.js
@@ -5,37 +5,47 @@ const axios = require('axios');
 
 module.exports = async function () {
     try {
-        const version = core.getInput('version');
+        let version = core.getInput('version');
+        let latestVersion;
         const url = `https://github.com/duckdb/duckdb/releases/download/${version}/duckdb_cli-linux-amd64.zip`
-        const wget = `wget ${url}`
-        const unzip = `unzip duckdb_cli-linux-amd64.zip`
-        const install = 'mkdir /opt/duckdb && mv duckdb /opt/duckdb && chmod +x /opt/duckdb/duckdb && sudo ln -s /opt/duckdb/duckdb /usr/bin/duckdb'
-        const check_version = 'duckdb --version'
-        const cleanup = 'rm duckdb_cli-linux-amd64.zip'
+        const wgetCmd = `wget ${url}`
+        const unzipCmd = `unzip duckdb_cli-linux-amd64.zip`
+        const installCmd = 'mkdir /opt/duckdb && mv duckdb /opt/duckdb && chmod +x /opt/duckdb/duckdb && sudo ln -s /opt/duckdb/duckdb /usr/bin/duckdb'
+        const checkVersionCmd = 'duckdb --version'
+        const cleanupCmd = 'rm duckdb_cli-linux-amd64.zip'
 
-        //let version_final = version;
+        core.info(`🔍 looking for the latest DuckDB version.`);
         const headers = {'Accept': 'application/vnd.github+json', 'X-GitHub-Api-Version': '2022-11-28'}
-        const res = await axios.get( 'https://api.github.com/repos/duckdb/duckdb/releases/latest', { headers: headers });
-
+        const res = await axios.get('https://api.github.com/repos/duckdb/duckdb/releases/latest', {headers: headers});
         if (res.status != 200) {
             core.error(`❌ Failed to get latest DuckDB version`);
-            core.setFailed(res.data);
+            core.setFailed(res.statusText);
         } else {
-            core.info(res.data.tag_name);
+            core.info(`✔️ Latest DuckDB version found is ${res.data.tag_name}.`);
+            latestVersion = res.data.tag_name;
         }
 
-        core.info(`📦 Install DuckDB version : ${version}`);
-        exec(`${wget} && ${unzip} && ${install} && ${cleanup} && ${check_version}`, (error, stdout, stderr) => {
+        if (version === 'latest') {
+            core.info(`📦 DuckDb latest version requested : ${latestVersion} will be installed.`);
+        }
+        else {
+            core.info(`📦 DuckDb ${version} requested.`);
+            if (version != latestVersion)
+                core.warning(`🆕 DuckDb ${version} is available.`);
+        }
+
+        core.info(`📥 Install DuckDB version : ${version}`);
+        exec(`${wgetCmd} && ${unzipCmd} && ${installCmd} && ${cleanupCmd} && ${checkVersionCmd}`, (error, stdout, stderr) => {
             if (error) {
-                core.error(`error: ${error.message}`);
+                core.error(`❌ ${error.message}`);
                 core.setFailed(error.message);
                 return;
             }
             if (stderr) {
-                core.error(`stderr: ${stderr}`);
+                core.error(`❌ ${stderr}`);
                 return;
             }
-            core.info("🚀 DuckDB successfully installed.");
+            core.info(`🚀 DuckDB ${version} successfully installed.`);
         });
     } catch (error) {
         core.setFailed(error.message);
