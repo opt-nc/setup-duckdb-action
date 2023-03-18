254fb70afa6278229eeaafd4b439a4f82d6aa382
Michèle BARRE <michele.barre@opt.nc>
2023-03-18 22:42:23 +1100

    ci: lint

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-/******/ /* webpack/runtime/compat */
+import './sourcemap-register.cjs';/******/ /* webpack/runtime/compat */
 /******/ 
 /******/ if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = new URL('.', import.meta.url).pathname.slice(import.meta.url.match(/^file:\/\/\/\w:/) ? 1 : 0, -1) + "/";
 /******/ 
@@ -7,3 +7,5 @@ var __webpack_exports__ = {};
 const action = require('./lib');
 (async function () {await action()})();
 
+
+//# sourceMappingURL=index.js.map
